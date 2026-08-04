cd885276e5287e1b4c5e42a6b609e890b095e48f
Ridam3010 <ridambaranwal30@gmail.com>
2026-08-04 20:20:42 +0530

    Task 2

--- src/main/java/com/maincrafts/landingpage/ContactController.java
+++ src/main/java/com/maincrafts/landingpage/ContactController.java
@@ -1,17 +1,36 @@
 package com.maincrafts.landingpage;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.List;
+
 @RestController
 public class ContactController {
+
+    @Autowired
+    private ContactRepository repo;
 
     @PostMapping("/contact")
     public String handleContact(@RequestParam String name,
                                 @RequestParam String email,
                                 @RequestParam String message) {
-        System.out.println("Name: " + name + ", Email: " + email + ", Message: " + message);
-        return "Form submitted successfully!";
+        
+        Contact newContact = new Contact();
+        newContact.setName(name);
+        newContact.setEmail(email);
+        newContact.setMessage(message);
+        
+        repo.save(newContact);
+        
+        return "Form submitted and saved to database successfully!";
+    }
+
+    @GetMapping("/contacts")
+    public List<Contact> getAllContacts() {
+        return repo.findAll();
     }
 }
